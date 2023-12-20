a2b6186c83c39bd99c62cb402c50ab38486f9383
mashimaro2010 <amnat.kaewtunkum@gmail.com>
2023-12-20 09:39:24 +0700

    สร้างชุดข้อมูลคนไข้นัด

--- index4.php
+++ index4.php
@@ -890,7 +890,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">ประเภทผู้ป่วย Refer</h1>
+            <h1 class="m-0">ประเภทผู้ป่วยนัด</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
